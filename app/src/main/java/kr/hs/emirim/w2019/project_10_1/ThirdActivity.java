ab517be3a8d1732e8e3240102b0e512aa6f7687c
choi ji won <w2019@e-mirim.hs.kr>
2021-07-19 10:24:31 +0900

    Change activity_main And Add ThirdActivity

---
+++ app/src/main/java/kr/hs/emirim/w2019/project_10_1/ThirdActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.w2019.project_10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
